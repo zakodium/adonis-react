2ee57cc9a352a902a1bc07cbfdafd51e1267db06
Michaël Zasso <targos@protonmail.com>
2020-11-17 21:51:01 +0100

    fix(types): expose share method

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 declare module '@ioc:Adonis/Core/HttpContext' {
   interface HttpContextContract {
     react: {
+      share(obj: Record<string, unknown>): void;
       render(Component: any, props?: Record<string, unknown>): string;
     };
   }
